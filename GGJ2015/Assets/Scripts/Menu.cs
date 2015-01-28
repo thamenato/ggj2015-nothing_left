8e678d3b151d2ec049fbc4bcbd9b83fa7da7e396
thamenato <thamenato@gmail.com>
2015-01-28 01:28:40 -0200

    Update on Menu
    Now the menu has buttons and a better layout :)

--- GGJ2015/Assets/Scripts/Menu.cs
+++ GGJ2015/Assets/Scripts/Menu.cs
@@ -1,11 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
 	
-	void Update () {
-		if(Input.anyKeyDown){
-			Application.LoadLevel("Cen01_Quarto");
-		}
+	GameObject mainMenu;
+	GameObject creditsMenu;
+
+
+	public void Start(){
+		var start = GameObject.Find ("start_btn").GetComponent<Button>();
+		var credits = GameObject.Find ("credits_btn").GetComponent<Button>();
+		var back = GameObject.Find ("back_btn").GetComponent<Button>();
+
+		start.onClick.AddListener(startGame);
+		credits.onClick.AddListener(showCredits);
+		back.onClick.AddListener(backMainMenu);
+
+		mainMenu = GameObject.Find ("mainMenu");
+		creditsMenu = GameObject.Find("creditsMenu");
+
+		creditsMenu.SetActive(false);
 	}
+	
+	public void startGame(){
+		Application.LoadLevel("Cen01_Quarto");
+	}
+
+	public void showCredits(){
+		mainMenu.SetActive(false);
+		creditsMenu.SetActive(true);
+	}
+	public void backMainMenu(){
+		creditsMenu.SetActive(false);
+		mainMenu.SetActive(true);
+	}
+
 }
