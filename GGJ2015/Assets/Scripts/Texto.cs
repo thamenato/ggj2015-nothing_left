67b7f4173230a6aacd84c8015183da08562a80f9
thamenato <thamenato@gmail.com>
2015-01-30 01:00:00 -0200

    Texto now in C#

---
+++ GGJ2015/Assets/Scripts/Texto.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Texto : MonoBehaviour {
+
+    int currentPosition = 0;
+    float Delay = 0.1f;
+    string Text = "";
+    string[] additionalLines;
+
+	// Use this for initialization
+	IEnumerator Start () {
+	    foreach(string s in additionalLines){
+            Text += "\n" + s;
+        }
+        while (true)
+        {
+            if (currentPosition < Text.Length)
+                guiText.text += Text[currentPosition++];
+            yield return new WaitForSeconds(Delay);
+        }
+	}
+	
+
+    void WriteText(string aText)
+    {
+        guiText.text = "";
+        currentPosition = 0;
+        Text = aText;
+    }
+
+}
